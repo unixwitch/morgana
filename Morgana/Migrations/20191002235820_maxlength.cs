b2f4bbe25417d2cf73f7e6f3199b3ea001739123
Felicity Tarnell <ft@le-Fay.ORG>
2019-10-03 00:59:11 +0100

    add migrations for maxlength

---
+++ Morgana/Migrations/20191002235820_maxlength.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Morgana.Migrations
+{
+    public partial class maxlength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "GuildId",
+                table: "GuildBadwords",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(ulong),
+                oldType: "INTEGER");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<ulong>(
+                name: "GuildId",
+                table: "GuildBadwords",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+        }
+    }
+}
